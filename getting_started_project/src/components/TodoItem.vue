4ddc1d5c00e42c00b4d7b7eea2fc73de755d3630
TowhidAust <towhidaustcse33@gmail.com>
2021-01-06 22:47:12 +0600

    bind events and todo ui

---
+++ getting_started_project/src/components/TodoItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+      <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+        <h3>
+            <input type="checkbox" v-on:change="markComplete">
+              {{todo.title}}
+              <button class="del">x</button>
+        </h3>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem", // this component name,
+    props: ["todo"],
+    methods: {
+        markComplete(){
+            // console.log(this.todo.completed);
+            let propsData = !this.todo.completed;
+            console.log(propsData)
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item{
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #cccccc;
+        margin: 5px;
+    }
+
+
+    .todo-item h3{
+         display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .is-complete{
+        text-decoration: line-through;
+    }
+
+    .del{
+        background: red;
+        border: none;
+        color: #f4f4f4;
+        padding: 3px;
+        border-radius: 3px;
+        cursor: pointer;
+        height: 25px;
+        width: 25px;
+    }
+</style>
---
+++ getting_started_project/src/components/Todos.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div v-for="(todo, index) in todos" v-bind:key="index">
+          <TodoItem v-bind:todo="todo"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem"
+export default {
+    name: "Todos", // this component name,
+    props: ["todos"],
+    components: {
+        TodoItem
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
